57c514a11116cc23763288073e22253b3d0f079f
Meet Indoriya <meetindoriya5@gmail.com>
2025-12-21 19:03:11 +0530

    Add files via upload

--- LAB - 1/WT_LAB_1A1.js
+++ LAB - 1/WT_LAB_1A1.js
@@ -2,7 +2,7 @@ const BirthDate = new Date("2006-12-11");
 const currentDate = new Date();
 
 console.log(currentDate);
-console.log (" Age: " + (currentDate.getFullYear() - BirthDate.getFullYear()));
-console.log (" Age: " + (currentDate.getMonth() - BirthDate.getMonth()));
-console.log (" Age: " + (BirthDate.getDate()- currentDate.getDate()));
+console.log(" Age ( Years ): " + (currentDate.getFullYear() - BirthDate.getFullYear()));
+console.log(" Age ( Month ): " + (currentDate.getMonth() - BirthDate.getMonth()));
+console.log(" Age ( Days ): " + (currentDate.getDate() - BirthDate.getDate()));
 
---
+++ LAB - 1/WT_LAB_1A3.js
@@ -0,0 +1,3 @@
+let otp = Math.random()*9
+let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+console.log("OTP : " + Math.floor(Math.random()*9) + Math.floor(Math.random()*9) + Math.floor(Math.random()*9) + Math.floor(Math.random()*9) + Math.floor(Math.random()*9) + Math.floor(Math.random()*9));
